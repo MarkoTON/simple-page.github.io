ecf1e76bfad90a1ae63560b88dd6b59716a321c5
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-01-25 17:41:41 +0100

    remove extra file

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function manuDrop() {
-  let element = document.getElementById("nav-links")
-
-  if (element.classList.contains("dontShow")) { 
-    element.setAttribute("class", "show");
-  } else {
-    element.setAttribute("class", "dontShow");
-  }
-}
